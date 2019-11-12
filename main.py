f8e86e0e960d267b72ecf1c6d5d2faeaa3df2c31
jdineen21 <jdineen21@gmail.com>
2019-11-12 12:52:04 +0000

    first commit completed

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import requests_cache
+
+from flask import Flask
+
+url_prefix = "https://api.spacexdata.com/v3/"
+
+requests_cache.install_cache('cache')
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    url = '%s%s' % (url_prefix, path)
+    r = requests.get(url)
+    return r.text
+
+if __name__ == '__main__':
+    app.run(host="192.168.0.26")
